eda34458a3ebf6964077cacff8e770c07a3efd23
fauzanananda-nice <fauzanananda8@gmail.com>
2026-06-15 18:50:47 +0700

    Logo dan Desc

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Tambahin baris ini bro:
-  allowedDevOrigins: ['192.168.50.153','10.10.44.150'],
+  allowedDevOrigins: ['192.168.50.153','10.10.44.150','192.168.1.9'],
 };
 
 export default nextConfig;
